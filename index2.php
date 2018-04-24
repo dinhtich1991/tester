3c2b55371a24337a96ba3fa5914ea9e8d71a88d0
conghau0401 <conghau0401@gmail.com>
2018-04-24 15:51:26 +0700

    step_git_2

--- index2.php
+++ index2.php
@@ -113,12 +113,12 @@
   </body>
 </html>
 
+Update (Nếu đã tồn tại file)
 UP: 
-
 1. git add --all
 2. git commit -m "name"
 3. git push origin master
-
-Down: 
-
+down: 
 1. git pull origin master
+Download (File chưa tồn tại ở máy)
+1. git clone tenfile.git
