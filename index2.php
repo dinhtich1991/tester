1ee87a045200f14ae775b5913a12df3ad2d8eaa1
conghau0401 <conghau0401@gmail.com>
2018-04-24 14:58:58 +0700

    step_up_git

--- index2.php
+++ index2.php
@@ -113,8 +113,12 @@
   </body>
 </html>
 
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-123123123
-123123123
-heheello
-1123457
+UP: 
+
+1. git add --all
+2. git commit -m "name"
+3. git push origin master
+
+Down: 
+
+1. git pull origin master
